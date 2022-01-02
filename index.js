5e6e4fb245d82c95fc0aa5abdce074d10b24cb36
Antonio <barbak2017@gmail.com>
2022-01-02 19:19:57 +0300

    youtube apis

--- index.js
+++ index.js
@@ -5,8 +5,9 @@ const LOL_STIKER = 'CAACAgIAAxkBAAIB3mHQqCcxD2vXmU50OgGIWCs-LBCRAAL6AAMIBwIAASxp
 const HELLO = /(Привет|здарова|здоров|здаров|hi|hello|Костя)/ig;
 const TY = /(Спасибо бот|спасибо робот|Спасибо)/ig
 const LOL = /(фхфх|ахахах|ахах|хехехе|хехе|ахаха|пхпхп|пхп|\)|хехе)/ig
+const NEWS = /(новости|новое|что там нового?|news|нового)/ig
 const { Telegraf } = require('telegraf')
-const { rundomHello, isHere } = require('./testlib')
+const { rundomHello, isHere, getList } = require('./testlib')
 
 let text, chatId; 
 let count = 0;
@@ -18,14 +19,25 @@ bot.start((ctx) => ctx.reply(`Детектор пидораса включен`)
 bot.on('sticker', (ctx) => console.log(ctx.message))
 bot.on('new_chat_members', (ctx) => ctx.reply(`Здоров ${ctx.message.from.first_name}`))
 
-bot.hears(HELLO, (ctx) => {
+//////////////////////////////////////////////////////////////////////////////////////////
+//                                    HEARS                                             //
+//////////////////////////////////////////////////////////////////////////////////////////
+
+bot.hears(HELLO, (ctx, next) => {
     ctx.reply(rundomHello())
+    return next()
 })
-bot.hears(TY, async (ctx) => {
+bot.hears(TY, async (ctx, next) => {
     await ctx.replyWithSticker(TY_STICKER)
+    return next()
 })
-bot.hears(LOL, async (ctx) => {
+bot.hears(LOL, async (ctx, next) => {
     await ctx.replyWithSticker(LOL_STIKER)
+    return next()
+})
+bot.hears(NEWS, async (ctx, next) => {
+    ctx.reply(await getList())
+    return next()
 })
 //////////////////////////////////////////////////////////////////////////////////////////
 //                                    COMMANDS                                          //
--- testlib.js
+++ testlib.js
@@ -1,3 +1,16 @@
+const {google} = require('googleapis')
+const YOUTUBE_API_TOKEN = 'AIzaSyAbUugM_mx1NJuq0bXTwIIDswhUr4aD0Gw';
+const YOUTUBE_VIDEO_URL = 'https://www.youtube.com/watch?v='
+const channelId= "UC4e_XPBiiIO4fo4_CucxQeg";
+const date = new Date()
+date.setDate(date.getDate() - 7)
+let videoId = "";
+let repo = "";
+let videoURL = "";
+
+// Authorization: Bearer [YOUR_ACCESS_TOKEN]
+// Accept: application/json
+
 const Hello = [
     `Здоров `,
     `Cколько лет, сколько зим! Здарова пидрило!`,
@@ -6,7 +19,6 @@ const Hello = [
     `Какие люди!`,
     `Cколько лет, сколько зим! Здарова пидрило!`
 ]
-
 
 function rundomHello() {
     const index = Math.floor(Math.random() * Hello.length)
@@ -20,5 +32,33 @@ function isHere (name, arr) {
     }
 }
 
+async function getList () {
+
+   return await google.youtube('v3').search.list({
+        key: YOUTUBE_API_TOKEN,
+        part: 'snippet',
+        publishedAfter: date,
+        maxResults: 10,
+        channelId: channelId
+    }).then((res)=>{
+        const index = res.data.pageInfo.totalResults - 1;
+        console.log(res.data.items[index])
+        videoId = res.data.items[index].id.videoId
+        if(videoId !== repo) {
+            repo = videoId;
+            videoURL = `${YOUTUBE_VIDEO_URL}${videoId}`
+            // console.log(videoURL)
+            return videoURL
+        }else{
+            return "Пока новой годноты не подьехало, пока...";
+        }
+    }).catch((e)=>{
+        console.log(e)
+    })
+}
+
+
+
 module.exports.rundomHello = rundomHello;
 module.exports.isHere = isHere;
+module.exports.getList = getList;
